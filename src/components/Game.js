f3c1bc8e2154d9a764c7e9356579fa75204a3e78
Arthur Guzman <alxguzmanc@gmail.com>
2021-12-12 23:08:28 -0500

    Add Homepage and Game components

---
+++ src/components/Game.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Game = () => (
+  <div>
+    <div>
+      <img src="img.png" alt="game logo" />
+    </div>
+    <h2>Game</h2>
+    <p>Game&apos;s description</p>
+  </div>
+);
+
+Game.propTypes = {
+
+};
+
+export default Game;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    <h1>LASTEST GAMES</h1>
+    <p>Please select a Game</p>
+  </div>
+);
+
+export default HomePage;
