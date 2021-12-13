a513d695015a41773f8ad05ffd84f82a8a43fff8
miguelesco <miuelangel1999@gmail.com>
2021-12-13 16:38:59 -0400

    test sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import HomePage from './pages/HomePage';
+import SideBar from './components/SideBar';
 
 const store = configureStore();
 
 function App() {
   return (
     <Provider store={store}>
+      <SideBar />
       <HomePage />
     </Provider>
   );
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,50 +1,47 @@
 import React from 'react';
+import {
+  CDBSidebar, CDBSidebarContent, CDBSidebarHeader,
+  CDBSidebarMenu, CDBSidebarMenuItem, CDBSidebarFooter,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 const SideBar = () => (
-  <div className="sidebar">
-    <div className="sidebar__header">
-      <div className="sidebar__header__logo">
-        <img src="https://res.cloudinary.com/dzq0gqbqy/image/upload/v1555122400/logo_wvxqjq.png" alt="logo" />
-      </div>
-      <div className="sidebar__header__title">
-        <h1>
-          <span>
-            <i className="fas fa-user-circle" />
-          </span>
-          <span>
-            <i className="fas fa-user-circle" />
-          </span>
-          <span>
-            <i className="fas fa-user-circle" />
-          </span>
-        </h1>
-      </div>
-    </div>
-    <div className="sidebar__body">
-      <div className="sidebar__body__nav">
-        <ul>
-          <li>
-            <a href="/">
-              <i className="fas fa-home" />
-              <span>Home</span>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <i className="fas fa-user" />
-              <span>Profile</span>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <i className="fas fa-cog" />
-              <span>Settings</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+  <div style={{ display: 'flex', height: '100%', overflow: 'scroll initial' }}>
+    <CDBSidebar textColer="#fff" backgroundColor="rgb(37, 90, 122)">
+      <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large" />}>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+      </CDBSidebarHeader>
+      <CDBSidebarContent className="sidebar-content">
+        <CDBSidebarMenu>
+          <NavLink exact to="/dashboard">
+            <CDBSidebarMenuItem icon="columns">
+              Transfer
+            </CDBSidebarMenuItem>
+          </NavLink>
+          <NavLink exact to="/dashboard">
+            <CDBSidebarMenuItem icon="columns">
+              Transfer
+            </CDBSidebarMenuItem>
+          </NavLink>
+          <NavLink exact to="/dashboard">
+            <CDBSidebarMenuItem icon="columns">
+              Transfer
+            </CDBSidebarMenuItem>
+          </NavLink>
+          <NavLink exact to="/dashboard">
+            <CDBSidebarMenuItem icon="columns">
+              Transfer
+            </CDBSidebarMenuItem>
+          </NavLink>
+        </CDBSidebarMenu>
+      </CDBSidebarContent>
+      <CDBSidebarFooter style={{ textAlign: 'center' }}>
+        <div className="sidebar-btn-wrapper" style={{ padding: '20px 5px' }}>
+          sidebar footer
+        </div>
+      </CDBSidebarFooter>
+    </CDBSidebar>
   </div>
 );
 
