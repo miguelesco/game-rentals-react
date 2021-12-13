e2b6608efa17d334fc56da0f4cd02be275ce0171
miguelesco <miuelangel1999@gmail.com>
2021-12-13 16:48:36 -0400

    test sidebar2

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,37 +3,26 @@ import {
   CDBSidebar, CDBSidebarContent, CDBSidebarHeader,
   CDBSidebarMenu, CDBSidebarMenuItem, CDBSidebarFooter,
 } from 'cdbreact';
-import { NavLink } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 const SideBar = () => (
   <div style={{ display: 'flex', height: '100%', overflow: 'scroll initial' }}>
     <CDBSidebar textColer="#fff" backgroundColor="rgb(37, 90, 122)">
-      <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large" />}>
-        <NavLink to="/dashboard">Dashboard</NavLink>
-      </CDBSidebarHeader>
+      <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large" />} />
       <CDBSidebarContent className="sidebar-content">
         <CDBSidebarMenu>
-          <NavLink exact to="/dashboard">
-            <CDBSidebarMenuItem icon="columns">
-              Transfer
-            </CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink exact to="/dashboard">
-            <CDBSidebarMenuItem icon="columns">
-              Transfer
-            </CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink exact to="/dashboard">
-            <CDBSidebarMenuItem icon="columns">
-              Transfer
-            </CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink exact to="/dashboard">
-            <CDBSidebarMenuItem icon="columns">
-              Transfer
-            </CDBSidebarMenuItem>
-          </NavLink>
+          <CDBSidebarMenuItem icon="columns">
+            Transfer
+          </CDBSidebarMenuItem>
+          <CDBSidebarMenuItem icon="columns">
+            Transfer
+          </CDBSidebarMenuItem>
+          <CDBSidebarMenuItem icon="columns">
+            Transfer
+          </CDBSidebarMenuItem>
+          <CDBSidebarMenuItem icon="columns">
+            Transfer
+          </CDBSidebarMenuItem>
         </CDBSidebarMenu>
       </CDBSidebarContent>
       <CDBSidebarFooter style={{ textAlign: 'center' }}>
