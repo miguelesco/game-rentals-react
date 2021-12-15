bfd46507be3522b31bcf2c171a02bb53775acfa4
miguelesco <miuelangel1999@gmail.com>
2021-12-15 16:48:44 -0400

    sidebar responsive

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   CDBSidebar, CDBSidebarContent, CDBSidebarHeader,
   CDBSidebarMenu, CDBSidebarMenuItem, CDBSidebarFooter,
+  CDBBtn as Button,
 } from 'cdbreact';
 import { Link, NavLink } from 'react-router-dom';
 import { FaFacebookF, FaTwitter, FaPinterestP } from 'react-icons/fa';
-// import PropTypes from 'prop-types';
-import '../assets/components_styles/sidebar.module.css';
+import PropTypes from 'prop-types';
+import style from '../assets/components_styles/sidebar.styles';
 
 const actives = {
   home: false,
@@ -22,46 +23,66 @@ const SetActive = (item) => {
   return actives[item] ? activeStyle : undefined;
 };
 
-const SideBar = () => (
-  <div style={{ display: 'flex', height: '100%', overflow: 'scroll initial' }}>
-    <CDBSidebar textColor="#333" backgroundColor="#fff">
-      <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large" />}>
-        <Link to="/home">Yorus Store</Link>
-      </CDBSidebarHeader>
-      <CDBSidebarContent className="sidebar-content">
-        <CDBSidebarMenu>
-          <CDBSidebarMenuItem icon="columns" active className={SetActive('home')}>
-            <NavLink exact="true" to="/home" style={({ isActive }) => (isActive ? SetActive('home') : undefined)}>
-              Juegos
+const SideBar = (props) => {
+  const { sidebarMobile, width } = props;
+  const [hideSideBarContent, setHideSideBarContent] = useState(false);
+  useEffect(() => {
+    if (width < 720) {
+      setHideSideBarContent(width < 720);
+    }
+  }, [width]);
+
+  const hamburgerClick = () => {
+    setHideSideBarContent(!hideSideBarContent);
+    sidebarMobile(hideSideBarContent);
+  };
+  return (
+
+    <div style={{ display: 'flex', height: '100%', overflow: 'scroll initial' }}>
+      <CDBSidebar textColor="#333" backgroundColor="#fff">
+        <CDBSidebarHeader prefix={(
+          <Button
+            style={style}
+            className="fa fa-bars fa-large border-none bg-transparent shadow-none"
+            onClick={hamburgerClick}
+          />
+  )}
+        >
+          <Link to="/home">Yorus Store</Link>
+        </CDBSidebarHeader>
+        <CDBSidebarContent className={hideSideBarContent && width < 720 ? 'd-none' : undefined}>
+          <CDBSidebarMenu>
+            <CDBSidebarMenuItem icon="columns" active className={SetActive('home')}>
+              <NavLink exact="true" to="/home" style={({ isActive }) => (isActive ? SetActive('home') : undefined)}>
+                Juegos
+              </NavLink>
+            </CDBSidebarMenuItem>
+            <NavLink exact="true" to="/" className="activeClicked">
+              <CDBSidebarMenuItem icon="columns">
+                Reservations
+              </CDBSidebarMenuItem>
             </NavLink>
-          </CDBSidebarMenuItem>
-          <NavLink exact="true" to="/" activeClassName="activeClicked">
-            <CDBSidebarMenuItem icon="columns">
-              Reservations
-            </CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink exact="true" to="/" activeClassName="activeClicked">
-            <CDBSidebarMenuItem icon="columns">
-              Sign Up
-            </CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink exact="true" to="/" activeClassName="activeClicked">
-            <CDBSidebarMenuItem icon="columns">
-              Sign Up
-            </CDBSidebarMenuItem>
-          </NavLink>
-        </CDBSidebarMenu>
-      </CDBSidebarContent>
-      <CDBSidebarFooter style={{ textAlign: 'center' }}>
-        <div className="sidebar-btn-wrapper" style={{ padding: '20px 5px' }}>
-          <FaFacebookF />
-          <FaTwitter />
-          <FaPinterestP />
-          <p>Reserved rights Yorus</p>
-        </div>
-      </CDBSidebarFooter>
-    </CDBSidebar>
-  </div>
-);
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+        <CDBSidebarFooter style={{ textAlign: 'center' }} className={hideSideBarContent && width < 720 ? 'd-none' : undefined}>
+          <div className="sidebar-btn-wrapper" style={{ padding: '20px 5px' }}>
+            <FaFacebookF />
+            <FaTwitter />
+            <FaPinterestP />
+            {
+              !hideSideBarContent ? <p>© 2021 Yorus Store. All rights reserved.</p>
+                : <p>© Yorus Store</p>
+            }
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+    </div>
+  );
+};
+
+SideBar.propTypes = {
+  sidebarMobile: PropTypes.func.isRequired,
+  width: PropTypes.number.isRequired,
+};
 
 export default SideBar;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CarouselContainer from '../components/CarouselContainer';
 import SideBar from '../components/SideBar';
 import style from '../assets/components_styles/homepage.module.css';
 
-const HomePage = () => (
-  <div className={style.homePage}>
-    <SideBar />
-    <CarouselContainer />
-  </div>
-);
+const HomePage = () => {
+  const [sidebarOpenMobile, setSidebarOpenMobile] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setWidth(window.innerWidth);
+    });
+  }, []);
+
+  const sidebarMobile = (hide) => {
+    if (width < 720) {
+      setSidebarOpenMobile(hide);
+    }
+  };
+
+  return (
+    <div className={`${style.homePage} ${sidebarOpenMobile && width < 720 ? 'd-grid h-100' : ''}`}>
+      <SideBar sidebarMobile={sidebarMobile} width={width} />
+      <CarouselContainer />
+    </div>
+  );
+};
 
 export default HomePage;
