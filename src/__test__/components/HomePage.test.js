7842032cbddd42719c88aa47bff25046b1c600c9
Arthur Guzman <alxguzmanc@gmail.com>
2021-12-20 14:48:18 -0500

    Add HomePage test structure

---
+++ src/__test__/components/HomePage.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import HomePage from '../../pages/HomePage';
+
+const setRender = () => {
+  const component = render(<HomePage />);
+  return component;
+};
+
+describe('Test Homepage component', () => {
+  // Arrange
+  // Act
+  // Assert
+});
