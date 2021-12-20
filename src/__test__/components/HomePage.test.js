3ff4281fce86b79ea9f8b90b522759cb7e871e73
Arthur Guzman <alxguzmanc@gmail.com>
2021-12-20 14:58:20 -0500

    Add test for checking content in HomePage

--- src/__test__/components/HomePage.test.js
+++ src/__test__/components/HomePage.test.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import configureStore from '../../store/store';
 import HomePage from '../../pages/HomePage';
-
-const setRender = () => {
-  const component = render(<HomePage />);
-  return component;
-};
 
 describe('Test Homepage component', () => {
   // Arrange
+  const store = configureStore();
+  const setRender = () => {
+    const component = render(
+      <Provider store={store}>
+        <HomePage />
+      </Provider>,
+    );
+    return component;
+  };
+  // Act
   let component;
   beforeEach(() => {
     component = setRender();
   });
-  // Act
   // Assert
+  test('Should render the title', () => {
+    const { getByText } = component;
+    expect(getByText('LASTEST GAMES')).toBeInTheDocument();
+  });
+
+  test('Should render the subtitle', () => {
+    const { getByText } = component;
+    expect(getByText('Please select a Game')).toBeInTheDocument();
+  });
 });
